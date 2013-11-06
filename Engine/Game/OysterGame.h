94c1ae23f1bd49f545d5d344fd70c778f89eb9e5
CarlDenisAndersen <carl.dennis.andersen@gmail.com>
2013-11-06 15:05:35 +0100

    Deleted old files

--- Engine/Game/OysterGame.h
+++
@@ -1,3 +0,0 @@
-#include "MoveAble.h"
-#include "CollisionHandler.h"
-#include "SateliteCamera.h"
--- Engine/Render/Lights.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Engine\Math\OysterMath.h"
-
-struct DirectionalLight
-{
-	Oyster::Math::Float4 Irradiance;
-	Oyster::Math::Float4 Direction;
-};
