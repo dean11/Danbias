220f1da8b6bad87bd74a29a9b4f20c266c24436b
Erik Persson <erikpersson583@gmail.com>
2013-11-26 08:56:31 +0100

    added GameMode class to level

---
+++ Code/GameLogic/GameMode.h
@@ -0,0 +1,17 @@
+#ifndef GAMEMODE_H
+#define GAMEMODE_H
+
+namespace GameLogic
+{
+
+	class GameMode
+	{
+		public:
+			GameMode(void);
+			~GameMode(void);
+		private:
+			//variabels that control what game rules the level runs on
+	};
+
+}
+#endif
