6e71b6ac545348bad3d04cdfa6101eb1cd8862e3
dean11 <carl.dennis.andersen@gmail.com>
2013-11-28 15:17:25 +0100

    Fixed merging errors

---
+++ Code/Misc/Thread/IThreadObject.h
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////////////////////////
+// Created by [Dennis Andersen] [2013]
+/////////////////////////////////////////////////////////////////////
+
+#ifndef MISC_I_THREAD_OBJECT_H
+#define MISC_I_THREAD_OBJECT_H
+
+
+
+namespace Oyster 
+{ 
+	namespace Thread
+	{
+		/**
+		*	Inherit this class to get threading compatibility.
+		*/
+		class IThreadObject
+		{
+		public:
+			/**
+			*	Override this to get notified when the thread is started.
+			*/
+			virtual void ThreadEntry()	{ }
+			/**
+			*	Override this to get notified when the thread is about to exit.
+			*/
+			virtual void ThreadExit()	{ }
+			/**
+			*	This function is required to get threading working.
+			*/
+			virtual bool DoWork			( ) = 0;
+		};
+	}
+}
+
+#endif // !MISC_I_THREAD_OBJECT_H
+
