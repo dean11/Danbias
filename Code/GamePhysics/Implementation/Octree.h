bddc3acad2dcc6a55b6bc7f1413d207d7bea6d7e
Robin Engman <robinengman@hotmail.com>
2013-11-29 09:07:52 +0100

    Changed from pointer to reference
    Did it in sample and visit functions

--- Code/GamePhysics/Implementation/Octree.h
+++ Code/GamePhysics/Implementation/Octree.h
@@ -50,9 +50,9 @@ namespace Oyster
 			void DestroyObject(::Utility::DynamicMemory::UniquePointer< ICustomBody > customBodyRef);
 
 			std::vector<ICustomBody*>& Sample(ICustomBody* customBodyRef, std::vector<ICustomBody*>& updateList);
-			std::vector<ICustomBody*>& Sample(Oyster::Collision3D::ICollideable* collideable, std::vector<ICustomBody*>& updateList);
+			std::vector<ICustomBody*>& Sample(const Oyster::Collision3D::ICollideable& collideable, std::vector<ICustomBody*>& updateList);
 			void Visit(ICustomBody* customBodyRef, VistorAction hitAction );
-			void Visit(Oyster::Collision3D::ICollideable* collideable, VistorAction hitAction );
+			void Visit(const Oyster::Collision3D::ICollideable& collideable, VistorAction hitAction );
 
 			ICustomBody* GetCustomBody(const unsigned int tempRef);
 
