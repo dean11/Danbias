61e1d20748e4608b0126c5ecbedf0bf5d9059876
Robin Engman <robinengman@hotmail.com>
2013-12-16 09:55:59 +0100

    Fixed const problem

--- Code/OysterPhysics3D/Ray.cpp
+++ Code/OysterPhysics3D/Ray.cpp
@@ -55,20 +55,20 @@ bool Ray::Intersects( const ICollideable &target ) const
 	}
 }
 
-bool Ray::Intersects( const ICollideable &target, const ::Oyster::Math::Float3 &worldPointOfContact ) const
+bool Ray::Intersects( const ICollideable &target, ::Oyster::Math::Float3 &worldPointOfContact ) const
 {
 	switch( target.type )
 	{
 	case Type_universe:
 		this->collisionDistance = 0.0f;
 		return true;
-	case Type_point: return Utility::Intersect( *this, *(Point*)&target, worldPointOfContact );
-	case Type_ray: return Utility::Intersect( *this, *(Ray*)&target, ((Ray*)&target)->collisionDistance, worldPointOfContact );
-	case Type_sphere: return Utility::Intersect( *this, *(Sphere*)&target, worldPointOfContact );
-	case Type_plane: return Utility::Intersect( *(Plane*)&target, *this, worldPointOfContact );
+	case Type_point: return Utility::Intersect( *this, *(Point*)&target, this->collisionDistance, worldPointOfContact );
+	case Type_ray: return Utility::Intersect( *this, *(Ray*)&target,  this->collisionDistance, ((Ray*)&target)->collisionDistance, worldPointOfContact );
+	case Type_sphere: return Utility::Intersect( *(Sphere*)&target, *this, this->collisionDistance, worldPointOfContact );
+	case Type_plane: return Utility::Intersect( *(Plane*)&target, *this, this->collisionDistance, worldPointOfContact );
 	// case Type_triangle: return false; // TODO: 
-	case Type_box_axis_aligned: return Utility::Intersect( *(BoxAxisAligned*)&target, *this, worldPointOfContact );
-	case Type_box: return Utility::Intersect( *(Box*)&target, *this, worldPointOfContact );
+	case Type_box_axis_aligned: return Utility::Intersect( *(BoxAxisAligned*)&target, *this, this->collisionDistance, worldPointOfContact );
+	case Type_box: return Utility::Intersect( *(Box*)&target, *this, this->collisionDistance, worldPointOfContact );
 	case Type_frustrum: return false; // TODO: 
 	default: worldPointOfContact = NULL;
 		return false;
