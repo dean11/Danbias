ba4732e350fe63ed4dd352075b51e2365ec06fe6
Dander7BD <dander.1981@gmail.com>
2014-01-29 10:33:46 +0100

    Added console output

--- Code/Physics lab/main.cpp
+++ Code/Physics lab/main.cpp
@@ -4,6 +4,8 @@
 #include "GFXAPI.h"
 #include "PhysicsAPI.h"
 
+#include <cstdio>
+
 using namespace ::Utility;
 using namespace ::Oyster;
 using namespace ::Oyster::Math3D;
@@ -27,6 +29,10 @@ struct Object
 
 // typedef LRESULT (CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK MessagePipe( HWND, UINT, WPARAM, LPARAM );
+
+void RedirectIOToConsole(); // copy pasted from http://www.cplusplus.com/forum/windows/58206/
+
+void OnCollisionResponse( const Physics::ICustomBody *proto, const Physics::ICustomBody *deuter, Float kineticEnergyLoss );
 
 int WINAPI WinMain( HINSTANCE thisInstance, HINSTANCE prevInst, PSTR cmdLine, int cmdShow)
 {
@@ -73,6 +79,8 @@ int WINAPI WinMain( HINSTANCE thisInstance, HINSTANCE prevInst, PSTR cmdLine, in
 			return 0;
 		}
 	}
+
+	RedirectIOToConsole();
 	
 	const Float graphicsPeriodicy = 1.0f / 60.0f;
 	Float graphicsTimeAccumulation = 0.0f;
@@ -114,6 +122,7 @@ int WINAPI WinMain( HINSTANCE thisInstance, HINSTANCE prevInst, PSTR cmdLine, in
 
 //		descCrate.centerPosition = Float4( 2.5, -0.5f, -0.5f, 1.0f );
 		descCrate.centerPosition = Float4::standard_unit_w;
+		descCrate.subscription_onCollisionResponse = OnCollisionResponse;
 		crate[1].phys = Physics::API::Instance().CreateRigidBody( descCrate ).Release();
 
 		for( unsigned int i = 0; i < StaticArray::NumElementsOf(crate); ++i )
@@ -240,4 +249,87 @@ LRESULT CALLBACK MessagePipe( HWND hWnd, UINT message, WPARAM wParam, LPARAM lPa
 	}
 
 	return 0;
+}
+
+void OnCollisionResponse( const Physics::ICustomBody *proto, const Physics::ICustomBody *deuter, Float kineticEnergyLoss )
+{
+	static int count = 0;
+	++count;
+	printf( "OnCollisionResponse::Count %d\n", count );
+	
+	Physics::ICustomBody::State state;
+	proto->GetState( state );
+	printf( "\tproto::Momentum(Linear, Angular)  %f %f\n", state.GetLinearMomentum().GetMagnitude(), state.GetAngularMomentum().GetMagnitude() );
+	Float4 w = Float4::null;
+	state.GetMomentOfInertia().CalculateAngularVelocity( Rotation(state.GetAngularAxis()), state.GetAngularMomentum(), w.xyz );	
+	printf( "\tproto::Velocity(Linear, Angular)  %f %f\n", state.GetLinearMomentum().GetMagnitude() / state.GetMass(), w.GetMagnitude() );
+	
+	Float4 sumLinear = Float4( state.GetLinearMomentum(), 0.0f ),
+		   sumAngular = Float4( state.GetAngularMomentum(), 0.0f );
+
+	deuter->GetState( state );
+	printf( "\tdeuter::Momentum(Linear, Angular) %f %f\n", state.GetLinearMomentum().GetMagnitude(), state.GetAngularMomentum().GetMagnitude() );
+	state.GetMomentOfInertia().CalculateAngularVelocity( Rotation(state.GetAngularAxis()), state.GetAngularMomentum(), w.xyz );	
+	printf( "\tdeuter::Velocity(Linear, Angular) %f %f\n", state.GetLinearMomentum().GetMagnitude() / state.GetMass(), w.GetMagnitude() );
+
+	sumLinear += Float4( state.GetLinearMomentum(), 0.0f );
+	sumAngular -= Float4( state.GetAngularMomentum(), 0.0f );
+
+	printf( "\tSumMomentum(Linear, Angular) %f %f\n", sumLinear.GetMagnitude(), sumAngular.GetMagnitude() );
+}
+
+// copy pasted from http://www.cplusplus.com/forum/windows/58206/
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
+void RedirectIOToConsole()
+{
+	// maximum mumber of lines the output console should have
+	static const WORD MAX_CONSOLE_LINES = 500;
+
+    int hConHandle;
+    long lStdHandle;
+    CONSOLE_SCREEN_BUFFER_INFO coninfo;
+    FILE *fp;
+
+	// allocate a console for this app
+    AllocConsole();
+
+	// set the screen buffer to be big enough to let us scroll text
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&coninfo);
+    coninfo.dwSize.Y = MAX_CONSOLE_LINES;
+    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE),coninfo.dwSize);
+
+	// redirect unbuffered STDOUT to the console
+    lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
+    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+
+    fp = _fdopen( hConHandle, "w" );
+
+    *stdout = *fp;
+
+    setvbuf( stdout, NULL, _IONBF, 0 );
+
+	// redirect unbuffered STDIN to the console
+
+    lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
+    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+
+    fp = _fdopen( hConHandle, "r" );
+    *stdin = *fp;
+    setvbuf( stdin, NULL, _IONBF, 0 );
+
+	// redirect unbuffered STDERR to the console
+    lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
+    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+
+    fp = _fdopen( hConHandle, "w" );
+
+    *stderr = *fp;
+
+    setvbuf( stderr, NULL, _IONBF, 0 );
+
+	// make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
+	// point to console as well
+    ::std::ios::sync_with_stdio();
 }
