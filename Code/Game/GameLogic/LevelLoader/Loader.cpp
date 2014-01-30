c78bbba3eb17f848c6eb32c4b44a86ca9ccb0e5a
Sam Mario Svensson <sammario.svensson@gmail.com>
2014-01-30 13:33:59 +0100

    GL- Removed commentet unnecessary code

--- Code/Game/GameLogic/LevelLoader/Loader.cpp
+++ Code/Game/GameLogic/LevelLoader/Loader.cpp
@@ -17,18 +17,6 @@ char* Loader::LoadFile(std::string fileName, int &size)
 	//convert from wstring to wchar then loads the file
 	char* buffer = (char*)OysterResource::LoadResource(temp.c_str(), Oyster::Resource::ResourceType::ResourceType_Byte_Raw, -1 , false);
 
-	//std::ifstream f;
-	//f.open(fileName, std::ios::binary);
-	//if (!f.is_open())
-	//	return 0;
-	//f.seekg(0, std::ifstream::end);
-	//size = f.tellg();
-	//f.seekg(0);
-	//char* buffer = new char[size];
-	//f.read(buffer, size);
-	//
-	//f.close();
-	//gets the size of the char buffer.
 	size = OysterResource::GetResourceSize(buffer);
 	return buffer;
 }
