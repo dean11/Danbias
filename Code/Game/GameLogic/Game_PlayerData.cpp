7b503bcb5233a60a339665beecc3142c601c9796
lindaandersson <lindaandersson92@hotmail.com>
2014-01-31 15:17:09 +0100

    GL - working with snap

--- Code/Game/GameLogic/Game_PlayerData.cpp
+++ Code/Game/GameLogic/Game_PlayerData.cpp
@@ -7,7 +7,7 @@ Game::PlayerData::PlayerData()
 {	
 	//set some stats that are appropriate to a player
 	Oyster::Physics::API::SimpleBodyDescription sbDesc;
-	sbDesc.centerPosition = Oyster::Math::Float3(10,350,0);
+	sbDesc.centerPosition = Oyster::Math::Float3(0,320,0);
 	sbDesc.size = Oyster::Math::Float3(4,7,4);
 	sbDesc.mass = 70;
 	//create rigid body
--- Code/Game/GameLogic/Level.cpp
+++ Code/Game/GameLogic/Level.cpp
@@ -41,7 +41,7 @@ void Level::InitiateLevel(float radius)
 	
 	// add box
 	API::SimpleBodyDescription sbDesc_TestBox;
-	sbDesc_TestBox.centerPosition = Oyster::Math::Float4(0,320,0,0);
+	sbDesc_TestBox.centerPosition = Oyster::Math::Float4(10,320,0,0);
 	sbDesc_TestBox.ignoreGravity = false;
 	sbDesc_TestBox.mass = 50;
 	sbDesc_TestBox.size = Oyster::Math::Float4(4,4,4,0);
