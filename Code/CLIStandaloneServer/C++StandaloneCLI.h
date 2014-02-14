e7cd44e75eb8eeca497a27d7dcccb6953c87949d
Pontus Fransson <pontus.fransson92@gmail.com>
2014-02-14 10:48:49 +0100

    Server - Added CLI project

---
+++ Code/CLIStandaloneServer/C++StandaloneCLI.h
@@ -0,0 +1,23 @@
+#ifndef CLISTANDALONESERVER_C++_Standalone_CLI_H
+#define CLISTANDALONESERVER_C++_Standalone_CLI_H
+
+#include "..\Game\GameServer\GameServerAPI.h"
+
+using namespace System;
+
+struct ServerInitDesc
+{
+	
+};
+
+public ref class CppStandaloneCLI
+{
+public:
+	
+
+protected:
+	DanBias::GameServerAPI* gameServer;
+
+};
+
+#endif
