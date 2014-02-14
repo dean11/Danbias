5defe7c0f762f0fb0409e99ab53c5c1de2612c8b
Erik Persson <erikpersson583@gmail.com>
2014-02-14 10:08:29 +0100

    GL - fixed compile errors

--- Code/Game/GameLogic/Portal.cpp
+++ Code/Game/GameLogic/Portal.cpp
@@ -9,13 +9,9 @@ Portal::Portal(void)
 	this->portalExit = Float3(0,0,0);
 }
 
-<<<<<<< HEAD
-Portal::Portal(Oyster::Physics::ICustomBody *rigidBody, void (*collisionFuncAfter)(Oyster::Physics::ICustomBody *proto,Oyster::Physics::ICustomBody *deuter,Oyster::Math::Float kineticEnergyLoss), ObjectSpecialType type, Oyster::Math::Float3 portalExit)
-	:StaticObject(rigidBody, collisionFuncAfter, type)
-=======
+
 Portal::Portal(Oyster::Physics::ICustomBody *rigidBody, void (*EventOnCollision)(Oyster::Physics::ICustomBody *proto,Oyster::Physics::ICustomBody *deuter,Oyster::Math::Float kineticEnergyLoss), ObjectSpecialType type,int objectID ,Oyster::Math::Float3 portalExit)
 	:StaticObject(rigidBody, EventOnCollision, type, objectID)
->>>>>>> GL - fixed constructors to ease creation of objects, also fixed weapon crash on destruction
 {
 	this->portalExit = portalExit;
 }
