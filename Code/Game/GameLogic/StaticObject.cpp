9fc8a8840f3c6d2310f20168a2a878ac0cf85aa9
Erik Persson <erikpersson583@gmail.com>
2014-02-14 12:03:29 +0100

    GL - fixed movement(pretty unstable but its working)

--- Code/Game/GameLogic/StaticObject.cpp
+++ Code/Game/GameLogic/StaticObject.cpp
@@ -14,25 +14,25 @@ StaticObject::StaticObject()
 StaticObject::StaticObject(Oyster::Physics::ICustomBody *rigidBody , void (*EventOnCollision)(Oyster::Physics::ICustomBody *proto,Oyster::Physics::ICustomBody *deuter,Oyster::Math::Float kineticEnergyLoss), ObjectSpecialType type, int objectID)
 	:Object(rigidBody, EventOnCollision, type, objectID)
 {
-	//use setMass(when it is made) and set the mass to 0 in order to ensure that the object is static
+	rigidBody->SetMass(0);
 }
 
 StaticObject::StaticObject(Oyster::Physics::ICustomBody *rigidBody , Oyster::Physics::ICustomBody::SubscriptMessage (*EventOnCollision)(Oyster::Physics::ICustomBody *proto,Oyster::Physics::ICustomBody *deuter,Oyster::Math::Float kineticEnergyLoss), ObjectSpecialType type, int objectID)
 	:Object(rigidBody, EventOnCollision, type, objectID)
 {
-	//use setMass(when it is made) and set the mass to 0 in order to ensure that the object is static
+	rigidBody->SetMass(0);
 }
 
 StaticObject::StaticObject(Oyster::Physics::ICustomBody *rigidBody , void (*EventOnCollision)(Oyster::Physics::ICustomBody *proto,Oyster::Physics::ICustomBody *deuter,Oyster::Math::Float kineticEnergyLoss), ObjectSpecialType type, int objectID, Oyster::Math::Float extraDamageOnCollision)
 {
 	this->extraDamageOnCollision = extraDamageOnCollision;
-	//use setMass(when it is made) and set the mass to 0 in order to ensure that the object is static
+	rigidBody->SetMass(0);
 }
 
 StaticObject::StaticObject(Oyster::Physics::ICustomBody *rigidBody , Oyster::Physics::ICustomBody::SubscriptMessage (*EventOnCollision)(Oyster::Physics::ICustomBody *proto,Oyster::Physics::ICustomBody *deuter,Oyster::Math::Float kineticEnergyLoss), ObjectSpecialType type, int objectID, Oyster::Math::Float extraDamageOnCollision)
 {
 	this->extraDamageOnCollision = extraDamageOnCollision;
-	//use setMass(when it is made) and set the mass to 0 in order to ensure that the object is static
+	rigidBody->SetMass(0);
 }
 StaticObject::~StaticObject(void)
 {
