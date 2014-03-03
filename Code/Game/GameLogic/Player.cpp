3bb6252caa93965c184de72484a83ffded724367
Dander7BD <dander.1981@gmail.com>
2014-03-03 10:33:16 +0100

    Calmed down player movement
    Was to easy to clip through walls

--- Code/Game/GameLogic/Player.cpp
+++ Code/Game/GameLogic/Player.cpp
@@ -15,10 +15,10 @@ const float AFFECTED_TIMER = 1.0f;
 
 // movement properties
 const Float dampening_factor = 0.2f,
-			forward_velocity = 100.0f,
-			backward_velocity = 50.0f,
-			strafe_velocity = 80.0f,
-			jump_velocity = 40.0f;
+			forward_velocity = 50.0f,
+			backward_velocity = 25.0f,
+			strafe_velocity = 40.0f,
+			jump_velocity = 25.0f;
 
 Player::Player()
 	:DynamicObject()
