f23ed2bae5873f2feba2334c1fee321182413651
Dander7BD <dander.1981@gmail.com>
2014-03-04 11:56:35 +0100

    Revert this! Debug trap in fumble

--- Code/Game/GameLogic/CollisionManager.cpp
+++ Code/Game/GameLogic/CollisionManager.cpp
@@ -250,6 +250,16 @@ using namespace GameLogic;
 		Math::Float3 deltaVelocity = realObjA->GetRigidBody()->GetLinearVelocity() - realObjB->GetRigidBody()->GetLinearVelocity();
 		Math::Float velocityNorm = deltaVelocity.Dot( deltaVelocity );
 
+		// HACK: debug trap
+		float v = realObjA->GetRigidBody()->GetLinearVelocity().GetMagnitude();
+		if( v >= 100.0f )
+			const char *breakpoint = "oscillated accelleration?";
+
+		// HACK: debug trap
+		v = realObjB->GetRigidBody()->GetLinearVelocity().GetMagnitude();
+		if( v >= 100.0f )
+			const char *breakpoint = "oscillated accelleration?";
+
 		static const Math::Float velocity_norm_threshold_interrupt_weapon = 3600.0f; // 60 m/s deltaVelocity
 		if( velocityNorm >= velocity_norm_threshold_interrupt_weapon )
 		{
