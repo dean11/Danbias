3e6bee46ba193fa94f7bc18781e5c154412815f2
Linda Andersson <lindaandersson92@hotmail.com>
2014-03-04 09:52:43 +0100

    change weapon mode

--- Code/Game/GameClient/GameClientState/GamingUI.cpp
+++ Code/Game/GameClient/GameClientState/GamingUI.cpp
@@ -173,10 +173,10 @@ void GamingUI::OnKeyPress(Enum::SAKI key, Keyboard* sender)
 		case SAKI_Space:	this->sharedData->network->Send( Protocol_PlayerJump() );
 		break;
 		// swap weapon to massDriver
-		//case SAKI_1:		this->sharedData->network->Send( Protocol_PlayerChangeWeapon( 0 ) );
+		case SAKI_1:		this->sharedData->network->Send( Protocol_PlayerChangeWeapon( 0 ) );
 			break; 
 		// swap weapon to shooting weapon
-		//case SAKI_2:		this->sharedData->network->Send( Protocol_PlayerChangeWeapon( 1 ) );
+		case SAKI_2:		this->sharedData->network->Send( Protocol_PlayerChangeWeapon( 1 ) );
 			break;
 		case SAKI_Escape:	this->nextState = GameStateUI::UIState_inGameMeny;
 		break;
