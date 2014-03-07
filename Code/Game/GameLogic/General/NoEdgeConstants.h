911eb89a9569dce939fbdf9bfd633b8a6505482f
Pontus Fransson <pontus.fransson92@gmail.com>
2014-03-07 15:19:08 +0100

    UtilityCooldown

--- Code/Game/GameLogic/General/NoEdgeConstants.h
+++ Code/Game/GameLogic/General/NoEdgeConstants.h
@@ -70,7 +70,7 @@ namespace NoEdgeConstants
 
 				const float PrimaryCooldown				= 0.2f;
 				const float SecondaryCooldown			= 0.0f;
-				const float MiddleCooldown				= 0.0f;
+				const float UtilityCooldown				= 0.1f;
 
 				const float MaxDistanceFromPlayer		= 5.0f;
 			}
